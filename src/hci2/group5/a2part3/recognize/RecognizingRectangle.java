5f2ad95c5194bbed3657074f74af0847330bb6ec
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-28 00:15:41 -0600

    Rectangle algorithm v1: it recognize a rectangle as long as a shape is closed, this algorithm checks the shape's bound values to determine rectangle's size.

--- src/hci2/group5/a2part3/recognize/RecognizingRectangle.java
+++ src/hci2/group5/a2part3/recognize/RecognizingRectangle.java
@@ -1,23 +1,78 @@
 package hci2.group5.a2part3.recognize;
 
-public class RecognizingRectangle extends Recognizing {
+import hci2.group5.a2part3.shape.Rectangle;
+import hci2.group5.a2part3.util.PointMaster;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This algorithm recognize a rectangle as long as a shape is closed. This algorithm checks
+ * the shape's bound values to determine rectangle's size.
+ *
+ */
+public class RecognizingRectangle extends Recognizing {
+	
+	private float left = Integer.MAX_VALUE, right = 0, top = 0, bottom = Integer.MAX_VALUE; // initial bound values
+	
+	private List<PointMaster> openCorners = new ArrayList<PointMaster>();
+	
+	private boolean initialDraw = true;
+	
 	@Override
 	public void touchDown(float x, float y) {
-		// TODO Auto-generated method stub
+		if (initialDraw ) {
+			// add first corner
+			openCorners.add(new PointMaster(x, y));
+			
+			return;
+		}
 		
+		closeOrAddCorner(x, y);
+		updateBounds(x, y);
 	}
 
 	@Override
 	public void touchMove(float x, float y) {
-		// TODO Auto-generated method stub
-		
+		updateBounds(x, y);
 	}
 
 	@Override
 	public void touchUp(float x, float y) {
-		// TODO Auto-generated method stub
+		if (initialDraw ) {
+			// not initial draw anymore, make "touchDown" work normally
+			initialDraw = false;
+		}
+		
+		closeOrAddCorner(x, y);
+		updateBounds(x, y);
+		
+		if (areCornersAllClosed()) {
+			doneRecognizing(new Rectangle(left, top, right, bottom));
+		}
 	}
 	
-	// NOTE: when done, call doneRecognizing(Rectangle blabla)
+	private boolean areCornersAllClosed() {
+		return ! initialDraw && openCorners.isEmpty();
+	}
+	
+	private void closeOrAddCorner(float x, float y) {
+		PointMaster p = new PointMaster(x, y);
+		for (PointMaster corner : openCorners) {
+			if ( corner.isRoughlyTheSame(p)) {
+				openCorners.remove(corner); // close corner
+				return;
+			}
+		}
+		
+		// failed to close a coner, add another open corner
+		openCorners.add(new PointMaster(x, y));
+	}
+
+	private void updateBounds(float x, float y) {
+		left = Math.min(left, x);
+		right = Math.max(right, x);
+		top = Math.max(top, y);
+		bottom = Math.min(bottom, y);
+	}
 }
