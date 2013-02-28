ca0be00f9adda0c2838e2e21f1ac3a5188f2abd9
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-27 23:05:28 -0600

    /shape/Rectangle.java: used to draw rectangle

--- src/hci2/group5/a2part3/shape/Rectangle.java
+++ src/hci2/group5/a2part3/shape/Rectangle.java
@@ -1,12 +1,31 @@
 package hci2.group5.a2part3.shape;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
 public class Rectangle implements Drawable {
 
+	private RectF rect;
+
+	public static Paint _paint;
+	static {
+		_paint = new Paint();
+		_paint.setAntiAlias(true);
+		_paint.setColor(Color.BLACK);
+		_paint.setStyle(Paint.Style.STROKE);
+		_paint.setStrokeJoin(Paint.Join.ROUND);
+		_paint.setStrokeCap(Paint.Cap.ROUND);
+		_paint.setStrokeWidth(2);
+	}
+	
+	public Rectangle(float left, float top, float right, float bottom) {
+		rect = new RectF(left, top, right, bottom);
+	}
+	
 	@Override
 	public void draw(Canvas canvas) {
-		// TODO Auto-generated method stub
-		
+		canvas.drawRect(rect, _paint);
 	}
 }
