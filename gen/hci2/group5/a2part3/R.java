5f4c09d3d9ec5234cc2a43fe7e4748da54d027f0
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-28 19:58:25 -0600

    Add toggle buttons to allow user to choose which shape to recognize
    Default is rectangle
    
    TODO
     - finish fill color button function

--- gen/hci2/group5/a2part3/R.java
+++ gen/hci2/group5/a2part3/R.java
@@ -14,7 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int canvasView=0x7f070000;
+        public static final int circle=0x7f070002;
+        public static final int color=0x7f070005;
+        public static final int line=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int rectangle=0x7f070004;
+        public static final int triangle=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/hci2/group5/a2part3/main/MainActivity.java
+++ src/hci2/group5/a2part3/main/MainActivity.java
@@ -1,16 +1,94 @@
 package hci2.group5.a2part3.main;
 
+import hci2.group5.a2part3.R;
+import hci2.group5.a2part3.recognize.RecognizingTypes;
+
+import java.util.Hashtable;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
+	private Hashtable<String, ToggleButton> _toggleButtons;
+	private CompoundButton _checkedButton;
+	
+	private CanvasView _canvasView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// this layout contain buttons and canvas view
+		setContentView(R.layout.activity_main);
 
-		CanvasView canvasView = new CanvasView(getApplicationContext());
-		setContentView(canvasView);
+		setUpToggleButtons();
+
+		// get canvas view
+		_canvasView = (CanvasView) findViewById(R.id.canvasView);
+		
+		// set default to recognize rectangle
+		_toggleButtons.get(RecognizingTypes.RECTANGLE.whatType()).setChecked(true);
+		_canvasView.setRecognizing(RecognizingTypes.RECTANGLE);
 	}
 
+	private void setUpToggleButtons() {
+		_toggleButtons = new Hashtable<String, ToggleButton>(4);
+		
+		// hash table with key that is tightly related to RecognizingTypes so that we can easily 
+		// find the RecognizingTypes when user click a toggle button is checked
+		_toggleButtons.put(RecognizingTypes.LINE.whatType(), (ToggleButton) findViewById(R.id.line));
+		_toggleButtons.put(RecognizingTypes.CIRCLE.whatType(), (ToggleButton) findViewById(R.id.circle));
+		_toggleButtons.put(RecognizingTypes.TRIANGLE.whatType(), (ToggleButton) findViewById(R.id.triangle));
+		_toggleButtons.put(RecognizingTypes.RECTANGLE.whatType(), (ToggleButton) findViewById(R.id.rectangle));
+		
+		setToggleButtonsOnCheckListeners(_toggleButtons);
+	}
+
+	private void setToggleButtonsOnCheckListeners(final Hashtable<String, ToggleButton> toggleButtons) {
+		for (ToggleButton toggleButton : toggleButtons.values()) {
+			
+			toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					if (isChecked) {
+						if (buttonView == _checkedButton) {
+							// avoid listener call when user wants to uncheck the current one and
+							// we call setChecked(true) that will call this method again...
+							return;
+						}
+						
+						_checkedButton = buttonView;
+						
+						// uncheck the checked one
+						for (ToggleButton toggleButton : toggleButtons.values()) {
+							if (toggleButton != buttonView && toggleButton.isChecked()) {
+								toggleButton.toggle();
+							}
+						}
+						
+						recognizingShapedChangedByUser(buttonView);
+					}
+					else {
+						// we don't want to uncheck the currently checked button
+						if (buttonView == _checkedButton) {
+							buttonView.setChecked(true);
+						}
+					}
+				}
+
+			});
+		}
+	}
+	
+	private void recognizingShapedChangedByUser(CompoundButton buttonView) {
+		for (RecognizingTypes recognizingType : RecognizingTypes.values()) {
+			if (buttonView == _toggleButtons.get(recognizingType.whatType())) {
+				_canvasView.setRecognizing(recognizingType);
+				return;
+			}
+		}
+	}
 }
