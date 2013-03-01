e4a1c1b00a6d4756558dd63868288360aad33266
roykxing <royk.xing@gmail.com>
2013-03-01 15:04:28 -0600

    changed triangle logic
    roy

--- src/hci2/group5/a2part3/recognize/RecognizingTriangle.java
+++ src/hci2/group5/a2part3/recognize/RecognizingTriangle.java
@@ -5,60 +5,58 @@ import hci2.group5.a2part3.util.PointMaster;
 
 public class RecognizingTriangle extends RecognizingByBoundingBoxAndCorners {
 
-	private PointMaster _p1,_p2,_p3,previous_p,secondPoint;
+	private PointMaster _p1,_p2,_p3;
+	private PointMaster pre_p, prepre_p,cur_p;
 	
-	private float scope;
 	private float ROUGH_RANGE = 1f;
 	
 	private boolean initialDraw = true;
-	private boolean getSecondPoint = false;
 	
 	@Override
 	public void touchDown(float x, float y) {
 		// TODO Auto-generated method stub
 		if(initialDraw){
 			_p1 = new PointMaster(x,y);
-			previous_p = new PointMaster(x,y);	
+			cur_p = _p1;
 		}
-		areCornersAllClosed();
 	}
 
 	@Override
 	public void touchMove(float x, float y) {
-		if(!getSecondPoint){
-			secondPoint = new PointMaster(x, y);
-			scope = getScop(_p1,secondPoint);
-			getSecondPoint = true;
-		}
-		
+		pre_p = cur_p;
+		cur_p = new PointMaster(x, y);
 		isStraightOrNot(x, y);
-		previous_p = new PointMaster(x, y);
-			
+		prepre_p = pre_p;
 	}
 
 	@Override
 	public void touchUp(float x, float y) {
 		if(_p2 != null && _p3 != null){
 			doneRecognizing(new Triangle(_p1, _p2, _p3));
-		}
-		
+		}	
 	}
 	
 	private void isStraightOrNot(float x, float y) {
-
-		float result = getScop(previous_p, secondPoint); 
-
-		if(result >= scope + ROUGH_RANGE)
-		{
-			if(_p2 == null)
-			{
-				_p2 = new PointMaster(x,y);
-				scope = result;
-				getSecondPoint = false;
-			}else if(_p3 == null){
-				_p3 = new PointMaster(x, y);
-				return;	
+		float scope_1,scope_2;
+		float result;
+		if(prepre_p != null){
+			scope_1 = getScop(prepre_p, pre_p);
+			scope_2 = getScop(pre_p, cur_p);
+			
+			result = scope_2 - scope_1;
+			
+			if(result > ROUGH_RANGE){
+				if(_p2 == null){
+					_p2 = pre_p;
+					prepre_p = pre_p;
+					
+				}else if(_p3 == null){
+					_p3 = new PointMaster(x, y);
+					return;
+				}
+				
 			}
+			
 		}
 		
 	}
